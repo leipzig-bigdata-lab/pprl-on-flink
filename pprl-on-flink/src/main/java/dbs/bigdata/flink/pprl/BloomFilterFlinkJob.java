1673276585db438374f2b0ce16605f4100d1bd57
Martin <mf97gady@studserv.uni-leipzig.de>
2016-05-17 16:51:47 +0200

    Added todo comments for our general pprl protocol / workflow.

--- pprl-on-flink/src/main/java/dbs/bigdata/flink/pprl/BloomFilterFlinkJob.java
+++ pprl-on-flink/src/main/java/dbs/bigdata/flink/pprl/BloomFilterFlinkJob.java
@@ -1,13 +1,10 @@
 package dbs.bigdata.flink.pprl;
 
-import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.FlatMapOperator;
 import org.apache.flink.api.java.operators.ReduceOperator;
-
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple5;
-
 import orestes.bloomfilter.BloomFilter;
 import orestes.bloomfilter.FilterBuilder;
 
@@ -34,6 +31,8 @@ public class BloomFilterFlinkJob {
 		
 		PersonDataLoader personLoader = new PersonDataLoader(env);
 		
+		// TODO maybe read records with all data fields and then map to a record with only relevant fields
+		
 		// build the n-grams
 		final int nGramValue = 2;
 		FlatMapOperator<Tuple5<String, String, String, String, String>, Tuple2<String, String>> tokens = 
@@ -56,5 +55,15 @@ public class BloomFilterFlinkJob {
 		bloomFilter.add("test2");
 		System.out.println(bloomFilter.asString());
 		
+		// TODO add (map) n-grams of each record to a bloom filter
+		
+		// TODO build blocks (use LSH for this) of bloom filters, where matches are supposed (flat map/reduce)
+		
+		// TODO compare candidate bloom filter pairs of the same block and return similarity values (flat map)
+		
+		// TODO check similarity values against a threshold and keep only those pairs with a similarity value
+		// greater then the threshold (reduce)
+		
+		// TODO maybe send or save the "matching pairs" to the parties or a file 
 	}
 }
